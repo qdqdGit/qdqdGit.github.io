a0893b6e9c9da0312c9138116db08f03aedd706d
QDQD <1042397515@qq.com>
2018-05-04 09:58:36 +0800

    适应更改

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 
-if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-    document.querySelector('body').style = "zoom: 100%;"
-} else {
-    document.querySelector('body').style = "zoom: 50%;"
+window.onload = function(){
+    if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        /*document.querySelector('body').style = "zoom: 50%;";*/
+        console.log('手机');
+    } else {
+       /*document.querySelector('body').style = "zoom: 100%;";*/
+        console.log('电脑');
+    }
+    console.log('加载完成');
 }
